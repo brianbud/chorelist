76011e667db8153e22ced7d19698ec774a3d333a
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-15 11:22:48 -0500

    delete all items

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const inputEl = document.getElementById("input-box");
 const addBtnEl = document.querySelector(".add-btn");
+const deleteBtnEl = document.querySelector(".delete-btn");
 const listItemsEl = document.querySelector(".list-items");
-
 addBtnEl.addEventListener("click", addToList);
+deleteBtnEl.addEventListener("click", deleteAllItems);
 
 function addToList() {
   let input = inputEl.value;
 
   listItemsEl.innerHTML += `<div class="items">${input}</div>`;
+}
+
+function deleteAllItems() {
+  listItemsEl.innerHTML = "";
 }
