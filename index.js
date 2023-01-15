4953066c8ddbdc1c7e7a224b36692747098dcbd1
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-15 10:43:15 -0500

    event listener to add btn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// javascript
+const inputEl = document.getElementById("input-box");
+const addBtnEl = document.querySelector(".add-btn");
+const containerEl = document.querySelector(".container");
+
+addBtnEl.addEventListener("click", addToList);
+
+function addToList() {}
