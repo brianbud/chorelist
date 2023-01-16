9070ec4b134a9f561fa363566d5b1588486a74a9
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-16 18:54:39 -0500

    save input to localstorage

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const deleteBtnEl = document.querySelector(".delete-btn");
 const listItemsEl = document.querySelector(".list-items");
 addBtnEl.addEventListener("click", addToList);
 deleteBtnEl.addEventListener("click", deleteAllItems);
+let myItems = [];
 
 function addToList() {
   let input = inputEl.value;
 
-  listItemsEl.innerHTML += `<div class="items">${input}</div>`;
+  if (input) {
+    myItems.push(input);
+    localStorage.setItem("myItems", JSON.stringify(myItems));
+    listItemsEl.innerHTML += `<div class="items">${input}</div>`;
+    inputEl.value = "";
+  } else {
+    return;
+  }
 }
 
 function deleteAllItems() {
