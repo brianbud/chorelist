f3275950ad9228a86ff007875fd695333f2d11ba
Brian Buddhadasa <brianbuddhadasa@gmail.com>
2023-01-16 20:43:40 -0500

    show list from localstorage

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const listItemsEl = document.querySelector(".list-items");
 addBtnEl.addEventListener("click", addToList);
 deleteBtnEl.addEventListener("click", deleteAllItems);
 let myItems = [];
+const itemsFromLocalStorage = JSON.parse(localStorage.getItem("myItems"));
+
+if (itemsFromLocalStorage) {
+  myItems = itemsFromLocalStorage;
+  render(myItems);
+}
+
+function render(items) {
+  let listItems = "";
+  for (let i = 0; i < items.length; i++) {
+    listItems += `<div class="items">${items[i]}</div>`;
+  }
+  listItemsEl.innerHTML = listItems;
+  console.log(listItemsEl);
+}
 
 function addToList() {
   let input = inputEl.value;
